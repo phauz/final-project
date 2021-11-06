fc6f38ce38141d855765097e5fb1b0e4e0043954
phauz <92535229+phauz@users.noreply.github.com>
2021-11-05 21:58:37 -0500

    committing

--- main.py
+++ main.py
@@ -7,29 +7,45 @@ pygame.font.init()
 
 SCREEN_W = 1400
 SCREEN_H = 800
-SCREEN = pygame.display.set_mode(SCREEN_W, SCREEN_H)
+win = pygame.display.set_mode(SCREEN_W, SCREEN_H)
 clock = pygame.time.Clock()
+steps = 10
 
 RUNNING = [pygame.image.load(os.path.join("images/outline", "_Run.png"))]
 
 
 class knight:
-    
+    X_POS = 80
+    Y_POS = 500
     def __init__(self):
         
         self.running = RUNNING
+        self.userInput = pygame.key.get_pressed()
         
-    def update(self. userInput):
-        if self.running:
-            self.run()
+        self.knight_rect = self.running.get_rect()
+        self.knight_rect.x = self.X_POS
+        self.knight_rect.y = self.X_POS
+        
+    pygame.sprite.Sprite.__init__(self):
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
+        
+    def running(self, userInput):
+    
+        if userInput[pygame.K_w] and [pygame.K_LSHIFT]:
             
             
-    def draw(self, SCREEN):
-        SCREEN.blit(self.image, (self))
+    
+            
+            
+    def draw(self, win):
+        win.blit(self.image, (self.knight_rect.x, self.knight_rect.y))
 
 
 
 run = True
+
 def main():
     
     while run: 
@@ -37,7 +53,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = false
                 
+    key_pressed = pygame.key.get_pressed()
                 
-    SCREEN.fill(255,255,255)
+                
+    win.fill(255,255,255)
     userInput = pygame.key.get_pressed()
+    knight.draw(win)
+    
+    clock.tick(30)
+    pygame.display.update()
     
