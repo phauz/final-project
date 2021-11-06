891eff1443dd96c00fcd46797d873a60b24ceb84
phauz <92535229+phauz@users.noreply.github.com>
2021-11-05 21:20:32 -0500

    committing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pygame.font.init()
 SCREEN_W = 1400
 SCREEN_H = 800
 SCREEN = pygame.display.set_mode(SCREEN_W, SCREEN_H)
+clock = pygame.time.Clock()
 
 RUNNING = [pygame.image.load(os.path.join("images/outline", "_Run.png"))]
 
@@ -17,3 +18,26 @@ class knight:
     def __init__(self):
         
         self.running = RUNNING
+        
+    def update(self. userInput):
+        if self.running:
+            self.run()
+            
+            
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self))
+
+
+
+run = True
+def main():
+    
+    while run: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = false
+                
+                
+    SCREEN.fill(255,255,255)
+    userInput = pygame.key.get_pressed()
+    
