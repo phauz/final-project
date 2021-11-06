68d104027d9e31f78fd59f4754612a1b12928201
phauz <92535229+phauz@users.noreply.github.com>
2021-11-05 21:16:46 -0500

    committing

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import pygame, sos, os
+import pygame, os, random, sys
 
+pygame.init()
+pygame.font.init()
+
+# screen variables
+
+SCREEN_W = 1400
+SCREEN_H = 800
+SCREEN = pygame.display.set_mode(SCREEN_W, SCREEN_H)
+
+RUNNING = [pygame.image.load(os.path.join("images/outline", "_Run.png"))]
+
+
+class knight:
+    
+    def __init__(self):
+        
+        self.running = RUNNING
